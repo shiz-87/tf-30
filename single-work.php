6a39d1ecd8c047bd2fc6f29d789af46bf342a016
shiz__87 <shizukayamamoto87@gmail.com>
2026-01-07 15:23:31 +0900

    feat: 現在表示している制作実績の関連記事を表示

--- single-work.php
+++ single-work.php
@@ -55,6 +55,61 @@
                         <div class="entry-work-btn">
                             <a class="btn" href="<?php echo esc_url(get_post_type_archive_link('work')); ?>">一覧に戻る</a>
                         </div><!-- /entry-work-btn -->
+
+                        <!-- 関連記事の表示 -->
+                        <?php
+                        $related_query = new WP_Query(
+                            array(
+                                'post_type' => 'work',
+                                'posts_per_page' => 3,
+                                'post__not_in' => array(get_the_ID()), // 現在の投稿を除外
+                                'tax_query' => array(
+                                    array(
+                                        'taxonomy' => 'genre',
+                                        'field' => 'term_id',
+                                        'terms' => $genre_term->term_id,
+                                    ),
+                                ),
+                            )
+                        );
+                        ?>
+
+                        <?php if ($related_query->have_posts()) : ?>
+                            <div class="entry-work-related">
+                                <div class="entry-work-related-head">関連記事</div><!-- /.entry-work-related-head -->
+                                <div class="entries entries-work entry-work-related-entries">
+                                    <?php while ($related_query->have_posts()) : ?>
+                                        <?php $related_query->the_post(); ?>
+
+                                        <!-- entry-item -->
+                                        <a href="<?php the_permalink(); ?>" <?php post_class(array('entry-item', 'entry-item-horizontal')); ?>>
+
+                                            <!-- entry-item-img -->
+                                            <div class="entry-item-img">
+                                                <?php
+                                                if (has_post_thumbnail()) {
+                                                    the_post_thumbnail();
+                                                } else {
+                                                    echo '<img src="' . esc_url(get_template_directory_uri()) . '/img/noimg.png" alt="">';
+                                                }
+                                                ?>
+                                            </div><!-- /entry-item-img -->
+
+                                            <!-- entry-item-body -->
+                                            <div class="entry-item-body">
+                                                <div class="entry-item-meta">
+                                                    <div class="entry-item-tag"><?php echo esc_html(get_the_terms(get_the_ID(), 'genre')[0]->name); ?></div><!-- /entry-item-tag -->
+                                                </div><!-- /entry-item-meta -->
+                                                <h2 class="entry-item-title"><?php the_title(); ?></h2><!-- /entry-item-title -->
+                                            </div><!-- /entry-item-body -->
+
+                                        </a><!-- /entry-item -->
+
+                                    <?php endwhile; ?>
+                                </div><!-- /.entry-work-related -->
+                            </div><!-- /.entry-work-related-entries -->
+                        <?php endif; ?>
+                        <?php wp_reset_postdata(); ?>
                     </article><!-- /entry -->
 
                 <?php endwhile; ?>
