f1bfbdd559dd6a392c0768eaf105048aa1a36b28
shiz__87 <shizukayamamoto87@gmail.com>
2026-01-07 09:04:04 +0900

    fix: CSSパスの記述の修正（get_template_directory_uri()に修正）

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_action('after_setup_theme', 'my_setup');
 function my_script_init()
 {
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css', array(), '5.8.2', 'all');
-    wp_enqueue_style('my', get_stylesheet_uri() . "/css/style.css", array(), filemtime(get_template_directory() . "/css/style.css"), 'all');
+    wp_enqueue_style('my', get_template_directory_uri() . "/css/style.css", array(), filemtime(get_template_directory() . "/css/style.css"), 'all');
     wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array('jquery'), filemtime(get_template_directory() . '/js/script.js'), true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
