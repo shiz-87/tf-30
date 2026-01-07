e39d094f835965d98f3dcc3cb0ba5d6ebccec160
shiz__87 <shizukayamamoto87@gmail.com>
2026-01-07 11:34:21 +0900

    feat: sigle.phpの作成、共通パーツの共通化

---
+++ sigle.php
@@ -0,0 +1,159 @@
+<?php get_header(); ?>
+
+<!-- content -->
+<div id="content">
+    <div class="inner">
+
+        <!-- primary -->
+        <main id="primary">
+
+            <!-- breadcrumb -->
+            <div class="breadcrumb">
+                <span property="itemListElement" typeof="ListItem">
+                    <a property="item" typeof="WebPage" href="/" class="home"><span property="name">ホーム</span></a>
+                    <meta property="position" content="1">
+                </span>
+                <i class="fas fa-angle-right"></i>
+                <span property="itemListElement" typeof="ListItem">
+                    <a property="item" typeof="WebPage" href="#" class="taxonomy category"><span
+                            property="name">カテゴリ名</span></a>
+                    <meta property="position" content="2">
+                </span>
+                <i class="fas fa-angle-right"></i>
+                <span class="post post-post current-item">記事のタイトルが入ります</span>
+            </div><!-- /breadcrumb -->
+
+            <!-- entry -->
+            <article class="entry">
+
+                <!-- entry-header -->
+                <div class="entry-header">
+                    <div class="entry-label"><a href="#">カテゴリ名</a></div><!-- /entry-item-tag -->
+                    <h1 class="entry-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</h1><!-- /entry-title -->
+
+                    <!-- entry-meta -->
+                    <div class="entry-meta">
+                        <time class="entry-published" datetime="2019-01-01">公開日 2019/1/1</time>
+                        <time class="entry-updated" datetime="2019-04-01">最終更新日 2019/4/1</time>
+                    </div><!-- /entry-meta -->
+
+                    <!-- entry-img -->
+                    <div class="entry-img">
+                        <img src="img/entry1.png" alt="">
+                    </div><!-- /entry-img -->
+
+                </div><!-- /entry-header -->
+
+                <!-- entry-body -->
+                <div class="entry-body">
+                    <p>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+
+                    <div id="toc_container">
+                        <p class="toc_title">この記事のコンテンツ</p>
+                        <ul class="toc_list">
+                            <li><a href="#i">見出しが入ります</a>
+                                <ul>
+                                    <li><a href="#">見出しが入ります</a></li>
+                                    <li><a href="#">見出しが入ります</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#i">見出しが入ります</a>
+                                <ul>
+                                    <li><a href="#">見出しが入ります</a></li>
+                                    <li><a href="#">見出しが入ります</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <h2>見出しが入ります</h2>
+                    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<a
+                            href="#">テキストリンクテキストリンク</a>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                    <h3>見出しが入ります</h3>
+                    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<a
+                            href="#">テキストリンクテキストリンク</a>テキストテキストテキストテキストテキスト<strong>テキストボールドテキストボールド</strong>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                    <h4>見出しが入ります</h4>
+                    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<a
+                            href="#">テキストリンクテキストリンク</a>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                    <h4>見出しが入ります</h4>
+                    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<a
+                            href="#">テキストリンクテキストリンク</a>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                    <div class="entry-btn"><a class="btn" href="">テキストテキスト</a></div><!-- /entry-btn -->
+                    <nav class="entry-links">
+                        <span class="post-page-numbers current">1</span>
+                        <a href="" class="post-page-numbers">2</a>
+                    </nav>
+                </div><!-- /entry-body -->
+
+
+                <div class="entry-tag-items">
+                    <div class="entry-tag-head">タグ</div><!-- /entry-tag-head -->
+                    <div class="entry-tag-item"><a href="">WordPress</a></div><!-- /entry-tag-item -->
+                    <div class="entry-tag-item"><a href="">コーディング</a></div><!-- /entry-tag-item -->
+                    <div class="entry-tag-item"><a href="">フリーランス</a></div><!-- /entry-tag-item -->
+                </div><!-- /entry-tag-items -->
+
+
+                <div class="entry-related">
+                    <div class="related-title">関連記事</div>
+
+                    <div class="related-items">
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                        <a class="related-item" href="">
+                            <div class="related-item-img"><img src="img/entry1.png" alt=""></div><!-- /related-item-img -->
+                            <div class="related-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div><!-- /related-item-title -->
+                        </a><!-- /related-item -->
+
+                    </div><!-- /related-items -->
+                </div><!-- /entry-related -->
+
+            </article> <!-- /entry -->
+        </main><!-- /primary -->
+
+        <?php get_sidebar(); ?>
+
+    </div><!-- /inner -->
+</div><!-- /content -->
+
+<?php get_footer(); ?>
